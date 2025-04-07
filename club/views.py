964ffbb7f10613fe16b94567a86333e18e44e4aa
Raihan Chowdhury <RaihanEXE99@gmail.com>
2025-04-07 14:24:19 +0600

    init

--- club/views.py
+++ club/views.py
@@ -3,6 +3,7 @@ from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
 from rest_framework import status
 from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView
+from rest_framework.response import Response
 
 from club.models import (
     Member,
@@ -88,3 +89,52 @@ class NoticeListAPIView(ListAPIView):
 class NoticeRetrieveAPIView(RetrieveAPIView):
     queryset = Notice.objects.all()
     serializer_class = NoticeSerializer
+
+class ContactCreateAPIView(CreateAPIView):
+    serializer_class = ContactRequestSerializer
+    queryset = ContactRequest.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        contact_request = serializer.save()
+
+        # Send email notification
+        try:
+            # Prepare context for email template
+            email_context = {
+                'contact': {
+                    'name': contact_request.name,
+                    'email': contact_request.email,
+                    'phone': contact_request.phone or 'N/A',
+                    'address': contact_request.address or 'N/A',
+                    'message': contact_request.message
+                }
+            }
+
+            # Email to club
+            club_email = EmailMessage(
+                subject=f'New Contact Request from {contact_request.name}',
+                body=render_to_string('email/contact_request_club.html', email_context),
+                from_email=f'DCC Programming Club <{settings.EMAIL_HOST_USER}>',
+                to=['dccpc.official@gmail.com'],
+                reply_to=[contact_request.email]
+            )
+            club_email.content_subtype = 'html'
+            club_email.send()
+
+            # Email to sender
+            sender_email = EmailMessage(
+                subject='We Received Your Message - DCC Programming Club',
+                body=render_to_string('email/contact_request_sender.html', email_context),
+                from_email=f'DCC Programming Club <{settings.EMAIL_HOST_USER}>',
+                to=[contact_request.email],
+            )
+            sender_email.content_subtype = 'html'
+            sender_email.send()
+
+        except Exception as e:
+            # Log the error, but still return a successful response
+            print(f"Email sending failed: {e}")
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
